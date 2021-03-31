2e2a562a993d57ef1de705ff032beb7ae80a16a4
Janio Fernandes <janiojr14@hotmail.com>
2021-03-31 19:40:04 -0300

    modularização do drag and drop

--- Assets/Atividades/DragAndDrop/Scripts/InsertField.cs
+++ Assets/Atividades/DragAndDrop/Scripts/InsertField.cs
@@ -6,7 +6,17 @@ using UnityEngine.UI;
 public class InsertField : MonoBehaviour
 {
     public int codigo;
+    public float NumCaracteres; //O tamanho (em caracteres) do seu field;
+    private float tamanhoBase; //O tamanho base de um caractere;
 
+    void Start()
+    {
+        tamanhoBase = 0.1f;
+        Transform t = GetComponent<Transform>();
+        t.localScale = (Vector3) new Vector3((tamanhoBase * NumCaracteres), tamanhoBase, 0);
+
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DragAndDropSprite col = collision.GetComponent<DragAndDropSprite>();
