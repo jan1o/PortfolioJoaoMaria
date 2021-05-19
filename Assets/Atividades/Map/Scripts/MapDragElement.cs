f42272de6474657d0e5ffcd117c7c8bf9484ebd0
Janio Fernandes <janiojr14@hotmail.com>
2021-05-19 09:44:56 -0300

    mapeamento base

---
+++ Assets/Atividades/Map/Scripts/MapDragElement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MapDragElement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cameraPrincipal;
+
+    private bool estaDentro;
+    private bool estaArrastando;
+
+    void Start()
+    {
+        cameraPrincipal = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (estaDentro && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            estaArrastando = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            estaArrastando = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (estaArrastando)
+        {
+            rb.MovePosition(cameraPrincipal.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        estaDentro = true;
+    }
+
+    private void OnMouseExit()
+    {
+        estaDentro = false;
+    }
+}
---
+++ Assets/Atividades/Map/Scripts/MapManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+
+    public int proximaCena;
+    public int menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void RegistrarAcerto(int valor)
+    {
+        
+    }
+
+    private void NextLevel()
+    {
+        SceneManager.LoadScene(proximaCena);
+    }
+
+
+}
