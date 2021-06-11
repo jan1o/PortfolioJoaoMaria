61c399721d60af02b0e013602f7d6cc5acd966af
Janio Fernandes <janiojr14@hotmail.com>
2021-06-11 09:18:17 -0300

    final version

---
+++ Assets/Fases/Scripts/InstrucoesPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrucoesPanel : MonoBehaviour
+{
+    public GameObject painelInstrucoes;
+
+    // Start is called before the first frame update
+
+    public void InvertPanelActivity()
+    {
+        painelInstrucoes.SetActive(!painelInstrucoes.activeSelf);
+    }
+}
